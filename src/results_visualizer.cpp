0a59f4f240159e47ab5455d0717cf407f442a243
nickcharron <nicholas.c.charron@gmail.com>
2023-03-07 23:01:31 -0800

    finalize viewer

--- src/results_visualizer.cpp
+++ src/results_visualizer.cpp
@@ -2,7 +2,9 @@
 
 #include <pcl/visualization/pcl_visualizer.h>
 
+#include <beam_filtering/Utils.h>
 #include <beam_utils/gflags.h>
+#include <beam_utils/kdtree.h>
 #include <beam_utils/pointclouds.h>
 
 DEFINE_string(input_directory, "",
@@ -12,51 +14,74 @@ DEFINE_validator(input_directory, &beam::gflags::ValidateDirMustExist);
 class ResultsVisualizer {
 public:
   struct ResultsData {
-    PointcloudPtr gt_cloud_in_world{std::make_shared<PointCloud>()};
+    PointCloudPtr gt_cloud_in_world{std::make_shared<PointCloud>()};
     std::vector<std::string> maps;
-  }
+  };
 
   explicit ResultsVisualizer(const std::string& input_dir) {
     LoadResultsData(input_dir);
     Setup();
   }
 
-  Run() {
-    for (const auto& map_path : data.maps) {
+  void Run() {
+    for (const auto& map_path : data_.maps) {
       PointCloud map;
+      BEAM_INFO("loading map: {}", map_path);
       pcl::io::loadPCDFile(map_path, map);
       if (map.empty()) {
         BEAM_ERROR("empty input map.");
         throw std::runtime_error{"empty input map"};
       }
 
+      viewer_.removePointCloud("CurrentMap");
       PointCloudPtr map_filtered = std::make_shared<PointCloud>();
       *map_filtered = FilterPointsCloseToGt(map);
-      pcl::visualization::PointCloudColorHandlerCustom<PointXYZ> col(
-          map_filtered, 255, 255, 255);
-      viewer_.addPointCloud<PointXYZ>(map_filtered, col, "CurrentMap");
+      pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> col(
+          map_filtered, 0, 255, 0);
+      viewer_.addPointCloud<pcl::PointXYZ>(map_filtered, col, "CurrentMap");
+      viewer_.setPointCloudRenderingProperties(
+          pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "CurrentMap");
+      next_scan_ = false;
+      while (!viewer_.wasStopped() && !next_scan_) { viewer_.spinOnce(); }
     }
   }
 
 private:
   void keyboardEventOccurred(const pcl::visualization::KeyboardEvent& event) {
-    if (event.getKeySym() == "n" && event.keyDown()) { next_scan_ = true; }
+    if (event.getKeySym() == "n" && event.keyDown()) {
+      BEAM_INFO("displaying next scan...");
+      next_scan_ = true;
+    }
   }
 
-  PointCloud FilterPointsCloseToGt(const PointCloud& map){
-    // TODO: fill this is
-    return map;
+  PointCloud FilterPointsCloseToGt(const PointCloud& map) {
+    PointCloud map_out;
+    for (int i = 0; i < map.size(); i++) {
+      std::vector<uint32_t> point_ids;
+      std::vector<float> point_distances;
+      kdtree_->nearestKSearch(map.at(i), 1, point_ids, point_distances);
+      if (!point_distances.empty() &&
+          point_distances.at(0) < max_distance_to_gt_) {
+        map_out.push_back(map.at(i));
+      }
+    }
+    return map_out;
   }
 
   void Setup() {
-    pcl::visualization::PointCloudColorHandlerCustom<PointXYZ> col(
-        data.gt_cloud_in_world, 255, 255, 255);
-    viewer_.addPointCloud<PointXYZ>(data.gt_cloud_in_world, col, "GTMap");
+    pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> col(
+        data_.gt_cloud_in_world, 255, 255, 255);
+    viewer_.addPointCloud<pcl::PointXYZ>(data_.gt_cloud_in_world, col, "GTMap");
+    viewer_.setPointCloudRenderingProperties(
+          pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 2, "GTMap");
     viewer_.addCoordinateSystem(1.0);
     std::function<void(const pcl::visualization::KeyboardEvent&)> keyboard_cb =
         [this](const pcl::visualization::KeyboardEvent& event) {
           keyboardEventOccurred(event);
         };
+    viewer_.registerKeyboardCallback(keyboard_cb);
+    kdtree_ =
+        std::make_unique<beam::KdTree<pcl::PointXYZ>>(*data_.gt_cloud_in_world);
   }
 
   void LoadResultsData(const std::string& dir) {
@@ -93,23 +118,26 @@ private:
         v[9], v[10], v[11], v[12], v[13], v[14], v[15];
 
     // filter input cloud
-    FilterParamsType filters{beam_filtering::FilterType::VOXEL,
-                             std::vector<double>{0.05, 0.05, 0.05}};
+    beam_filtering::FilterParamsType filter{
+        beam_filtering::FilterType::VOXEL,
+        std::vector<double>{0.05, 0.05, 0.05}};
+    std::vector<beam_filtering::FilterParamsType> filters{filter};
     PointCloud cloud_filtered =
-        beam_filtering::FilterPointCloud<PointXYZ>(gt_cloud_in, filters);
+        beam_filtering::FilterPointCloud<pcl::PointXYZ>(gt_cloud_in, filters);
 
-    pcl::transformPointCloud(cloud_filtered, data_.gt_cloud_in_world,
+    pcl::transformPointCloud(cloud_filtered, *data_.gt_cloud_in_world,
                              T_World_GtCloud);
 
     // load results
     nlohmann::json J2;
     std::string results_file = beam::CombinePaths(dir, "results_summary.json");
-    if (!beam::ReadJson(gt_cloud_pose, J2)) {
+    BEAM_INFO("Reading results json: {}", results_file);
+    if (!beam::ReadJson(results_file, J2)) {
       throw std::runtime_error{"Invalid gt_cloud_pose json"};
     }
     if (!J2.contains("map_names")) {
       throw std::runtime_error{
-          "invalid results file, no field named map_namse"};
+          "invalid results file, no field named map_names"};
     }
     std::vector<std::string> map_names = J2["map_names"];
     std::string map_dir = beam::CombinePaths(dir, "gt_maps");
@@ -120,10 +148,14 @@ private:
 
   ResultsData data_;
   bool next_scan_{false};
+  double max_distance_to_gt_{2};
+  std::unique_ptr<beam::KdTree<pcl::PointXYZ>> kdtree_;
   pcl::visualization::PCLVisualizer viewer_;
 };
 
 int main(int argc, char* argv[]) {
-  ResultsVisualizer(FLAGS_input_directory);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  ResultsVisualizer viz(FLAGS_input_directory);
+  viz.Run();
   return 0;
 }
